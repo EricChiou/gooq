cc403f753273a18750803a732dafd81210efb3ea
EricChiou <miatlab@gmail.com>
2020-10-15 20:59:09 +0800

    add trigonometric func to Function

--- trigonometric.go
+++ trigonometric.go
@@ -1,37 +1,37 @@
 package gooq
 
-func Sin(number string) string {
+func (f *Function) Sin(number string) string {
 	return "SIN(" + number + ")"
 }
 
-func Asin(number string) string {
+func (f *Function) Asin(number string) string {
 	return "ASIN(" + number + ")"
 }
 
-func Cos(number string) string {
+func (f *Function) Cos(number string) string {
 	return "COS(" + number + ")"
 }
 
-func Acos(number string) string {
+func (f *Function) Acos(number string) string {
 	return "ACOS(" + number + ")"
 }
 
-func Tan(number string) string {
+func (f *Function) Tan(number string) string {
 	return "TAN(" + number + ")"
 }
 
-func Atan(number string) string {
+func (f *Function) Atan(number string) string {
 	return "ATAN(" + number + ")"
 }
 
-func Cot(number string) string {
+func (f *Function) Cot(number string) string {
 	return "COT(" + number + ")"
 }
 
-func Degree(number string) string {
+func (f *Function) Degree(number string) string {
 	return "DEGREE(" + number + ")"
 }
 
-func Radians(number string) string {
+func (f *Function) Radians(number string) string {
 	return "RADIANS(" + number + ")"
 }
